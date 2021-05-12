c053df8937ff3c45d512417bca4f1b093a7fe27a
Andrea Grillo <grilloandrea6@gmail.com>
2021-05-12 23:06:32 +0200

    MotorTest error

--- MotorTest.ino
+++ MotorTest.ino
@@ -8,11 +8,12 @@ Movement movement;
 void setup() {
   Serial.begin(9600);
   movement.begin();
-}
-
-void loop() {
   for(int i = 0; i < 4; i++) {
     movement.go(FORWARD,MAX_SPEED,5);
     movement.turn(LEFT,MAX_SPEED,2);
   }
+}
+
+void loop() {
+  
 }
