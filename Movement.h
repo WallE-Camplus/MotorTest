eac2043c35b91e28cb385e038843fc503d2f4ec5
Marco Fincato <marcofincato777@gmail.com>
2021-05-19 10:07:34 +0200

    Correct motor name and order

--- Movement.h
+++ Movement.h
@@ -29,10 +29,10 @@ class Movement {
     void turn(bool direction, int speed, int theta);
     void goTurning(bool avance_direction, bool turn_direction, int speed, int distance, int theta);
   private:
-    Motor *front_left = new Motor(M1_PWM, M1_IN1, M1_IN2);
-    Motor *front_right = new Motor(M2_PWM, M2_IN1, M2_IN2);
-    Motor *rear_left = new Motor(M3_PWM, M3_IN1, M3_IN2);
-    Motor *rear_right = new Motor(M4_PWM, M4_IN1, M4_IN2);
+    Motor *rear_right = new Motor(M1_PWM, M1_IN1, M1_IN2); //RR
+    Motor *front_right = new Motor(M2_PWM, M2_IN1, M2_IN2); //FR
+    Motor *front_left = new Motor(M3_PWM, M3_IN1, M3_IN2); //FL
+    Motor *rear_left = new Motor(M4_PWM, M4_IN1, M4_IN2); //RL
     IMU imu;
 
 
